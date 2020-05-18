269c19b73a071ff4595a5e729ba3bafbafa0ab4f
Ilya <stepanov.ilya1@gmail.com>
2020-05-18 15:49:48 +0300

    sort-strings solved

--- 01-javascript-data-types/1-sort-strings/index.js
+++ 01-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,8 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+	const sortedArray = arr.slice();
 
+	sortedArray.sort( (a, b) => a.localeCompare(b, 'ru', {caseFirst: 'upper'}) );
+	return param === 'asc' ? sortedArray : sortedArray.reverse();
 }
